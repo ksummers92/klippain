96c2cef98f0326338a59a7ec23e237f877e517d1
ksummers92 <66363414+ksummers92@users.noreply.github.com>
2024-07-13 11:51:29 -0500

    fix

--- install.sh
+++ install.sh
@@ -79,7 +79,7 @@ function check_download {
 
     if [ ! -d "${FRIX_CONFIG_PATH}" ]; then
         echo "[DOWNLOAD] Downloading Klippain repository..."
-        if git -C $frixtemppath clone -b $frixbranchname https://github.com/klsummers92/klippain.git $frixreponame; then
+        if git -C $frixtemppath clone -b $frixbranchname https://github.com/ksummers92/klippain.git $frixreponame; then
             printf "[DOWNLOAD] Download complete!\n\n"
         else
             echo "[ERROR] Download of Klippain git repository failed!"
